78c8f4485b52e7af33b61876c72e08034a367e66
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-08-31 17:37:41 +0100

    Add files via upload

--- netscan.py
+++ netscan.py
@@ -16,10 +16,12 @@ def ping(host):
         print(f"Tempo limite de 2 segundos excedido para o host {host}.")
 n = 0
 i=127
+separetor="-----------------------------------------------------------"
 host_to_ping = "192.168.1."
 host_to_ping2 = host_to_ping 
 print("\033c\033[47;34m")
 if __name__ == "__main__":
     for n in range(1,i):
         host_to_ping2 = host_to_ping + str(n)
+        print(separetor)
         ping(host_to_ping2)
