dd3c8ff79a3c779357e07f796b4ddaa085e386c3
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-09-01 06:05:38 +0100

    Add files via upload

--- Linux.py
+++ Linux.py
@@ -1,10 +1,14 @@
 
-import subprocess
-import time
+import os
+
 
 def ping(host):
     
-    result = subprocess.call('ping '+host+' -c '+'1',shell=True) 
+    result = os.popen('ping '+host+' -c '+'1')
+    outputs=result.read()
+    if outputs.find("1 received")>0:
+        print(separetor)
+        print (host) 
 n = 0
 i=127
 separetor="-----------------------------------------------------------"
@@ -14,5 +18,4 @@ print("\033c\033[47;34m")
 if __name__ == "__main__":
     for n in range(1,i):
         host_to_ping2 = host_to_ping + str(n)
-        print(separetor)
         ping(host_to_ping2)
